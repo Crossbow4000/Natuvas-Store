a2b323fba9ee5d65e0713cbcde75d2624275bb66
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-09-12 14:52:02 -0500

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,12 @@ export default function Home({ printfulResponse }) {
         <title>Create Next App</title>
       </Head>
 
-      {printfulResponse.result.map((item, i) => {
+      {printfulResponse.result.map((item, i) => 
         <div>
           <h1>{item.name}</h1>
           <img src={item.thumbnail_url} alt={"An image of " + item.name}/>
         </div>
-      })}
+      )}
 
       {JSON.stringify(printfulResponse.result)}
       
