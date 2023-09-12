4d98206d346a51cd465df3b25889f45c77b08380
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-09-12 14:38:43 -0500

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      printfulResponse: printfulResponse.json.toString(),
+      printfulResponse: printfulResponse.json().toString(),
     },
   };
 }
