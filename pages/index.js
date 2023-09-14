90dea0f564ca5350ad55031fa482574aef06b537
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-09-14 14:41:29 -0500

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -21,17 +21,17 @@ export default function Home({ printfulResponse }) {
   );
 }
 
-export async function getStaticProps() {
-  const printfulApiKey = await process.env.PRINTFUL;
-  const printfulResponse = await fetch('https://api.printful.com/store/products', {
-    method: 'GET',
-    headers: {'Authorization': 'Bearer ' + printfulApiKey,}
-  })
-  .then(response => { return response.json() })
+// export async function getStaticProps() {
+//   const printfulApiKey = await process.env.PRINTFUL;
+//   const printfulResponse = await fetch('https://api.printful.com/store/products', {
+//     method: 'GET',
+//     headers: {'Authorization': 'Bearer ' + printfulApiKey,}
+//   })
+//   .then(response => { return response.json() })
 
-  return {
-    props: {
-      printfulResponse: printfulResponse,
-    },
-  };
-}
+//   return {
+//     props: {
+//       printfulResponse: printfulResponse,
+//     },
+//   };
+// }
