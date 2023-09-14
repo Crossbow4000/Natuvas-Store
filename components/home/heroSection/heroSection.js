d80a56d12a5c8de115bd106f5335794eaf6f91d8
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-09-14 14:19:35 -0500

    1 file modified, 1 added, 1 deleted

--- components/home/heroSection/heroSection.js
+++ components/home/heroSection/heroSection.js
@@ -1,6 +1,6 @@
 import styles from './heroSection.module.css';
 import homeStyles from '../home.module.css';
-import backgroundImage from './files/heroSectionBackground.jpeg'
+import backgroundImage from './files/heroSectionBackground.jpg'
 
 export default function HeroSection() {
   return (
