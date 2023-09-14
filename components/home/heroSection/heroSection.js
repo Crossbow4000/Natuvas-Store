ac7370ea1663a3c47f679e7fca45d51ae7e044fd
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-09-14 14:36:11 -0500

    1 binaries uploaded

--- components/home/heroSection/heroSection.js
+++ components/home/heroSection/heroSection.js
@@ -1,5 +1,6 @@
 import styles from './heroSection.module.css';
 import homeStyles from '../home.module.css';
+import image from './heroSectionBackground.jpg'
 
 export default function HeroSection() {
   return (
