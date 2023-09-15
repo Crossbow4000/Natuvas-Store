ccac439c9b2a82d21639c37768d1fc834a20cad6
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-09-15 14:57:09 -0500

    2 files modified

--- components/home/heroSection/heroSection.js
+++ components/home/heroSection/heroSection.js
@@ -1,5 +1,6 @@
 import styles from './heroSection.module.css';
 import homeStyles from '../home.module.css';
+import Link from 'next/link';
 
 export default function HeroSection() {
   return (
@@ -7,7 +8,7 @@ export default function HeroSection() {
       <div className={styles.heroSectionElementsWrapper}>
         <h1 className={styles.heroSectionTitle}>Natuvas</h1>
         <h2 className={styles.heroSectionSubtitle}>Hang the beauty of nature on your wall.</h2>
-        <a className={styles.heroSectionButton}>Shop All</a>
+        <Link className={styles.heroSectionButton} href="/store">Shop All</Link>
       </div>
     </div>
   )
