f35505f0233c10d75f43aeacf4d05930301c559d
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-09-15 14:55:03 -0500

    3 files modified, 1 added

--- components/home/heroSection/heroSection.js
+++ components/home/heroSection/heroSection.js
@@ -7,6 +7,7 @@ export default function HeroSection() {
       <div className={styles.heroSectionElementsWrapper}>
         <h1 className={styles.heroSectionTitle}>Natuvas</h1>
         <h2 className={styles.heroSectionSubtitle}>Hang the beauty of nature on your wall.</h2>
+        <a className={styles.heroSectionButton}>Shop All</a>
       </div>
     </div>
   )
