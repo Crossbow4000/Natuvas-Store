b17ddb133c6394c5f02e1e4bce681a5420a1dcc1
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-09-15 11:14:59 -0500

    1 file modified

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} onLoad={window.scrollTo(0, 150)} />;
 }
 
 export default MyApp;
