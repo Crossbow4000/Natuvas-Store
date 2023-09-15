3e8cede0918f24d9ed98806335eea12ccd3e1bd5
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-09-15 11:09:33 -0500

    2 files modified

--- components/home/navigationBar/navigationBar.js
+++ components/home/navigationBar/navigationBar.js
@@ -4,7 +4,7 @@ import homeStyles from '../home.module.css';
 export default function NavigationBar() {
   return (
     <div className={styles.navigationBar}>
-
+      <h1 className={styles.navigationBarTitle}>Natuvas</h1>
     </div>
   )
 }
