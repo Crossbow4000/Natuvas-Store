8b3d0fc53c1a33ac2cb7d89135cb52cf92796e6f
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-09-15 11:00:05 -0500

    2 files modified

--- components/home/navigationBar/navigationBar.js
+++ components/home/navigationBar/navigationBar.js
@@ -1,2 +1,10 @@
 import styles from './navigationBar.module.css';
 import homeStyles from '../home.module.css';
+
+export default function NavigationBar() {
+  return (
+    <div className={styles.navigationBar}>
+
+    </div>
+  )
+}
