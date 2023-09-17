6f0ae51d5755b9f820508baeca325122535316b5
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-09-17 16:53:07 -0500

    2 files modified

--- components/home/welcomeSection/welcomeSection.js
+++ components/home/welcomeSection/welcomeSection.js
@@ -5,6 +5,13 @@ export default function WelcomeSection() {
   return (
     <div className={styles.welcomeSection}>
       <h2 className={styles.welcomeSectionTitle}>Welcome</h2>
+      <p className={styles.welcomeSectionParagraph}>
+        Treat yourself to a high-quality canvas with a stunning nature print.​
+        <br />
+        <br />
+        <br />
+        Whatever part of nature you love to explore, whether that's the ocean, a forest, or even a desert, our selection of over 100 high quality nature canvases is sure to have a print that will express your special place's beauty.
+      </p>
     </div>
   )
 }
