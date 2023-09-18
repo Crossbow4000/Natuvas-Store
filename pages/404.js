af2ff8a3ec372974115c642c0b1ff7e378e7f31e
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-09-18 11:18:48 -0500

    1 file modified

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,14 @@
+import { useEffect, useState } from 'react';
+
 export default function CountNotBeFound() {
+  const [referrer, setReferrer] = useState('');
+  useEffect(() => { setReferrer(document.referrer); },[]);
+
+
   return (
     <div>
       <h1>401</h1>
-      <h2>This page could not be found</h2>
+      <h2>The page {referrer} could not be found</h2>
     </div>
   )
 }
