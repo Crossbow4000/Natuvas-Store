d1b50e97a4745a85e22a2a19c3d68f6dd8729e9f
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-09-18 19:11:53 -0500

    1 file modified

--- components/home/featuredSection/featuredSection.js
+++ components/home/featuredSection/featuredSection.js
@@ -5,7 +5,8 @@ export default function FeaturedSection({ featuredItems }) {
   return (
     <div className={styles.featuredSection}>
       <h2 className={styles.featuredSectionTitle}>Featured</h2>
-      <p className={styles.p}>{JSON.stringify(featuredItems)}</p>
+      <p className={styles.p}>{featuredItems[0].name}</p>
+      <p className={styles.p}>{featuredItems[1].name}</p>
     </div>
   )
 }
