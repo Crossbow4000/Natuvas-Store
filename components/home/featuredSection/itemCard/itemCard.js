09c26bc1cb2edca3682b0959e735147a4e627378
Crossbow4000 <62087695+Crossbow4000@users.noreply.github.com>
2023-09-21 18:02:11 -0500

    2 files modified

--- components/home/featuredSection/itemCard/itemCard.js
+++ components/home/featuredSection/itemCard/itemCard.js
@@ -3,7 +3,7 @@ import styles from './itemCard.module.css';
 export default function ItemCard({ itemName, thumbnailUrl }) {
   return (
     <div className={styles.itemCard}>
-      <div className={styles.itemCardImage} style={{backgroundImage: `url(${thumbnailUrl})`, backgroundSize: 'cover'}} ></div>
+      <div className={styles.itemCardImage} style={{backgroundImage: `url(${thumbnailUrl})`}} ></div>
       <p className={styles.itemCardName}>{itemName}</p>
     </div>
   )
